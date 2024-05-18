f500b5974fb690fd4f829aed9998a4075c3b8c50
Morgan <lethuaut.morgan@gmail.com>
2024-05-18 21:31:46 +0200

    added form logic and errors

--- scripts/form.js
+++ scripts/form.js
@@ -25,16 +25,17 @@
 // #\-EXPORTS-\
 
     // --THIS
-    export default function form_init() { form_set() }
+    export default function form_init() { form_set() } // initialize
 
 
 // #\-CONSTANTES-\
 
     // --THIS
-    const FORM = document.getElementById('form')
+    const
+    FORM       = document.getElementById('form'),
+    FORM_TESTS = form_getTests()
 
     // --INSIDE
-    const FORM_DATA = (FORM ?? document).querySelectorAll('.formData')
 
 
 // #\-VARIABLES-\
@@ -47,17 +48,80 @@
     // --SET
     function form_set() { form_setEvents() }
 
-    function form_setEvents() { FORM?.addEventListener('submit', form_eSubmit) }
+    function form_setEvents() { FORM?.addEventListener('submit', form_eSubmit) } // adds a 'submit' event to 'form'
 
     // --GET
+    function form_getTests() // returns an array of tests to be performed to validate the form
+    {
+        return [...(FORM ?? document).querySelectorAll('.formData')].reduce((accumulator, e) => // retrieves all "formdata" elements and reduced according to "input" requiring validation
+        {
+            const INPUTS = [...e?.querySelectorAll('input') ?? []] // retrieves all "formdata input"
+
+            if (INPUTS.length)
+            {
+                const TEST = form_getTest(INPUTS[0].id) // each "formdata" is associated with a test, here we check for the first "input"
+
+                if (TEST) accumulator.push( // added test to accumulator
+                {
+                    formdata: e,
+                    test    : TEST.bind(INPUTS)
+                })
+            }
+
+            return accumulator
+        }, [])
+    }
+
+    function form_getTest(id = '') // checks among a list of ids whether the "input" is associated with a test
+    {
+        switch (id)
+        {
+            case    'first'    :
+            case    'last'     : return name_test
+            case    'email'    : return email_test
+            case    'quantity' : return quantity_test
+            case    'location1':
+            case    'location2':
+            case    'location3':
+            case    'location4':
+            case    'location5':
+            case    'location6': return location_test
+            case    'checkbox1': return checkbox1_test
+            default            : return void ''
+        }
+    }
 
     // --UPDATES
 
     // --TESTS
-    function form_testInput()
+    function form_test() // after "form" submission or execute all tests for current values
     {
+        for (let {formdata, test} of FORM_TESTS)
+        {
+            try
+            {
+                test()
 
+                formdata?.removeAttribute('data-error')
+            }
+            catch ({message}) { return {formdata, message} } // in the case of a failed test, we return an error object
+        }
     }
+
+    function name_test() { if (this[0].value?.length < 2) throw new Error('Minimum 2 caractères.') }
+
+    function email_test() { if (!/^\S+@\S+\.\S+$/.test(this[0].value)) throw new Error('Adresse email invalide.') } // simple email RegExp
+
+    function quantity_test()
+    {
+        const VALUE = parseInt(this[0].value, 10)
+
+        if (isNaN(VALUE) || VALUE < 0) throw new Error('Renseignez une valeur numérique positive.')
+    }
+
+    function location_test() { if (this.every(e => !e.checked)) throw new Error('Sélectionnez une localisation.') }
+
+    function checkbox1_test() { if (!this[0].checked) throw new Error('Veuillez accepter les conditions d\'utilisation.') }
 
 
 //=======@EVENTS|
@@ -65,9 +129,14 @@
     // --*
     function form_eSubmit(e)
     {
-        e.preventDefault()
+        const ERROR = form_test()
 
-        form_testInput()
+        if (ERROR) // in the event of an error, form submission is blocked and the error displayed
+        {
+            e.preventDefault()
+
+            ERROR.formdata?.setAttribute('data-error', ERROR.message)
+        }
     }
 
 
